637d3a8165861e1b2b3a4062f137d86cf252810c
Mohammad Hilman Ali Reza <hilmanali14@gmail.com>
2024-06-09 21:36:19 +0700

    day-9 menangkap data dari exmapke API JSON

---
+++ assests/js/contact.js
@@ -0,0 +1,38 @@
+function submitData() {
+  const inputName = document.getElementById("inputName").value;
+  const inputEmail = document.getElementById("inputEmail").value;
+  const inputPhone = document.getElementById("inputPhone").value;
+  const inputSubject = document.getElementById("inputSubject").value;
+  const inputMessage = document.getElementById("inputMessage").value;
+
+  // Perkondisian
+  if (inputName == "") {
+    alert("Name harus diisi"); // Kondisi 1
+  } else if (inputEmail == "") {
+    alert("Email Harus Diisi"); // kondisi 2
+  } else if (inputPhone == "") {
+    alert("Phone Number tidak boleh K0s0n9"); //kondisi 3
+  } else if (inputSubject == "") {
+    alert("Subject tidak boleh kosong"); // kondisi 4
+  } else if (inputMessage == "") {
+    alert("Message tidak boleh kosong"); // kondisi 5
+  }
+  
+
+  // untuk perkondisian kerangkanya tidak harus menggunakan if, else if, dan else
+  // kita bisa menggunakan if aja, atau else if aja, atau if else aja
+  // yang tidak boleh adalah untuk kondisi pertama tidak menggunakan if
+  // jadi if adalah kode wajib yang digunakan untuk permulaan sebuah perkondisian
+
+  //   ketika dalam perkondisian kondisi yang tsudah terpenuhi maka akan mengecek perkondisian dibawahnya
+
+  console.log(
+    `Name : ${inputName}\nEmail : ${inputEmail}\nPhone : ${inputPhone}\nSubject : ${inputSubject}\nMessage : ${inputMessage}`
+  );
+
+  const myemail = "hilmanali14@gmail.com";
+
+  let a = document.createElement("a");
+  a.href = `mailto:${myemail}?subject=${inputSubject}&body=Hello my name ${inputName}, and my number ${inputPhone} ${inputMessage}`;
+  a.click();
+}
